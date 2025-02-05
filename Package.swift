9c7c62450312c86cd306154f114d92917931b035
Vedant Gurav <hello@vedantgurav.com>
2025-02-05 15:27:27 +0000

    Use alternate swift-syntax

--- Package.swift
+++ Package.swift
@@ -15,35 +15,20 @@ let package = Package(
     .library(name: "Perception", targets: ["Perception"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"511.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
+    .package(url: "https://github.com/sjavora/swift-syntax-xcframeworks.git", "509.0.0"..<"511.0.0")
   ],
   targets: [
     .target(
       name: "Perception",
       dependencies: [
         "PerceptionMacros",
-        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
-    ),
-    .testTarget(
-      name: "PerceptionTests",
-      dependencies: ["Perception"]
     ),
 
     .macro(
       name: "PerceptionMacros",
       dependencies: [
-        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-      ]
-    ),
-    .testTarget(
-      name: "PerceptionMacrosTests",
-      dependencies: [
-        "PerceptionMacros",
-        .product(name: "MacroTesting", package: "swift-macro-testing"),
+        .product(name: "SwiftSyntaxWrapper", package: "swift-syntax-xcframeworks")
       ]
     ),
   ]
